ba7a7ed836256e5b1c0cd55fc34cb3a0e72fc865
Muhammad Adnan Rizki Issanjaya <73287823+MAdnan220@users.noreply.github.com>
2021-05-30 20:40:49 +0700

    Add files via upload

---
+++ admin/main.php
@@ -0,0 +1,381 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>D'Creativ</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/d'creativ.png">
+
+    <!-- CSS
+	============================================ -->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="assets/css/vendor/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/themify-icons.css">
+    <link rel="stylesheet" href="assets/css/vendor/cryptocurrency-icons.css">
+
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/plugins.css">
+
+    <!-- Helper CSS -->
+    <link rel="stylesheet" href="assets/css/helper.css">
+
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body class="skin-dark">
+
+    <div class="main-wrapper">
+
+
+        <!-- Header Section Start -->
+        <div class="header-section">
+            <div class="container-fluid">
+                <div class="row justify-content-between align-items-center">
+
+                    <!-- Header Logo (Header Left) Start -->
+                    <div class="header-logo col-auto">
+                        <a href="index.html">
+                            <img src="assets/images/logo/logo.png" alt="">
+                            <img src="assets/images/logo/logo.png" class="logo-light" alt="">
+                        </a>
+                    </div><!-- Header Logo (Header Left) End -->
+
+                    <!-- Header Right Start -->
+                    <div class="header-right flex-grow-1 col-auto">
+                        <div class="row justify-content-between align-items-center">
+
+                            <!-- Side Header Toggle & Search Start -->
+                            <div class="col-auto">
+                                <div class="row align-items-center">
+
+                                    <!--Side Header Toggle-->
+                                    <div class="col-auto"><button class="side-header-toggle"><i class="zmdi zmdi-menu"></i></button></div>
+
+                                    <!--Header Search-->
+                                    <div class="col-auto">
+
+                                        <div class="header-search">
+
+                                            <button class="header-search-open d-block d-xl-none"><i class="zmdi zmdi-search"></i></button>
+
+                                            <div class="header-search-form">
+                                                <form action="#">
+                                                    <input type="text" placeholder="Search Here">
+                                                    <button><i class="zmdi zmdi-search"></i></button>
+                                                </form>
+                                                <button class="header-search-close d-block d-xl-none"><i class="zmdi zmdi-close"></i></button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div><!-- Side Header Toggle & Search End -->
+
+                            <!-- Header Notifications Area Start -->
+                            <div class="col-auto">
+
+                                <ul class="header-notification-area">
+
+                                    <!--User-->
+                                    <li class="adomx-dropdown col-auto">
+                                        <a class="toggle" href="#">
+                                            <span class="user">
+                                                <span class="avatar">
+                                                    <img src="assets/images/avatar/avatar-1.jpg" alt="">
+                                                    <span class="status"></span>
+                                                </span>
+                                                <span class="name">Madison Howard</span>
+                                            </span>
+                                            <!-- Dropdown -->
+                                            <div class="adomx-dropdown-menu dropdown-menu-user">
+                                                <div class="head">
+                                                    <h5 class="name"><a href="#">Madison Howard</a></h5>
+                                                    <a class="mail" href="#">mailnam@mail.com</a>
+                                                </div>
+                                                <div class="body">
+                                                    <ul>
+                                                        <li><a href="#"><i class="zmdi zmdi-account"></i>Profile</a></li>
+                                                        <li><a href="#"><i class="zmdi zmdi-email-open"></i>Inbox</a></li>
+                                                        <li><a href="#"><i class="zmdi zmdi-wallpaper"></i>Activity</a></li>
+                                                    </ul>
+                                                    <ul>
+                                                        <li><a href="#"><i class="zmdi zmdi-settings"></i>Setting</a></li>
+                                                        <li><a href="../index.php"><i class="zmdi zmdi-lock-open"></i>Sing out</a></li>
+                                                    </ul>
+                                                    <ul>
+                                                        <li><a href="#"><i class="zmdi zmdi-paypal"></i>Payment</a></li>
+                                                        <li><a href="#"><i class="zmdi zmdi-google-pages"></i>Invoice</a></li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+
+                                    </li>
+
+                                </ul>
+
+                            </div><!-- Header Notifications Area End -->
+
+                        </div>
+                    </div><!-- Header Right End -->
+
+                </div>
+            </div>
+        </div><!-- Header Section End -->
+        <!-- Side Header Start -->
+        <div class="side-header show">
+            <button class="side-header-close"><i class="zmdi zmdi-close"></i></button>
+            <!-- Side Header Inner Start -->
+            <div class="side-header-inner custom-scroll">
+
+                <nav class="side-header-menu" id="side-header-menu">
+                    <ul>
+                        <li class="has-sub-menu"><a href="#"><i class="ti-home"></i> <span>Dashboard</span></a>
+                            <ul class="side-header-sub-menu">
+                                <li><a href="main.php"><span>E-commerce (Default)</span></a></li>
+                            </ul>
+                        </li>
+
+
+                        <li class="has-sub-menu"><a href="index.php"><i class="ti-shopping-cart"></i> <span>E-commerce</span></a>
+                            <ul class="side-header-sub-menu">
+                                <li><a href="add-product.php"><span>Add Product</span></a></li>
+                                <li><a href="edit-product.php"><span>Edit Produk</span></a></li>
+                                <li><a href="invoice-list.php"><span>Invoice List</span></a></li>
+                                <li><a href="invoice-details.php"><span>Invoice Details</span></a></li>
+                                <li><a href="order-list.php"><span>Order List</span></a></li>
+                                <li><a href="order-details.php"><span>Order Details</span></a></li>
+                                <li><a href="manage-products.php"><span>Manage Products</span></a></li>
+                            </ul>
+                        </li>
+                        <li class="has-sub-menu"><a href="index.php"><i class="fa fa-book"></i> <span>Training</span></a>
+                            <ul class="side-header-sub-menu">
+                                <li><a href="add-Training.php"><span>Add Training</span></a></li>
+                                <li><a href="data-Training.php"><span>Data Training</span></a></li>
+                                <li><a href="Approvel-Training.php"><span>Approvel Training</span></a></li>
+                            </ul>
+                        </li>
+
+                        <li class="has-sub-menu"><a href="index.php"><i class="fa fa-rss-square"></i> <span>Blog</span></a>
+                            <ul class="side-header-sub-menu">
+                                <li><a href="add-BLog.php"><span>Add BLog</span></a></li>
+                                <li><a href="manage-BLog.php"><span>Data BLog</span></a></li>
+
+                            </ul>
+                        </li>
+
+
+                        <li class="has-sub-menu"><a href="#"><i class="ti-lock"></i> <span>Authentication</span></a>
+                            <ul class="side-header-sub-menu">
+                                <li><a href="login.html"><span>login</span></a></li>
+                                <li><a href="register.html"><span>register</span></a></li>
+                                <li><a href="author-profile.html"><span>Profile</span></a></li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                </nav>
+
+            </div><!-- Side Header Inner End -->
+        </div><!-- Side Header End -->
+
+        <!-- Content Body Start -->
+        <div class="content-body">
+
+            <!-- Page Headings Start -->
+            <div class="row justify-content-between align-items-center mb-10">
+
+                <!-- Page Heading Start -->
+                <div class="col-12 col-lg-auto mb-20">
+                    <div class="page-heading">
+                        <h3>Dashboard <span>/ eCommerce</span></h3>
+                    </div>
+                </div><!-- Page Heading End -->
+
+                <!-- Page Button Group Start -->
+                <div class="col-12 col-lg-auto mb-20">
+                    <div class="page-date-range">
+                        <input type="text" class="form-control input-date-predefined">
+                    </div>
+                </div><!-- Page Button Group End -->
+
+            </div><!-- Page Headings End -->
+
+            <!-- Top Report Wrap Start -->
+            <div class="row">
+                <!-- Top Report Start -->
+                <div class="col-xlg-3 col-md-6 col-12 mb-30">
+                    <div class="top-report">
+
+                        <!-- Head -->
+                        <div class="head">
+                            <h4>Total Visitor</h4>
+                            <a href="#" class="view"><i class="zmdi zmdi-eye"></i></a>
+                        </div>
+
+                        <!-- Content -->
+                        <div class="content">
+                            <h5>Todays</h5>
+                            <h2>100,560.00</h2>
+                        </div>
+
+                        <!-- Footer -->
+                        <div class="footer">
+                            <div class="progess">
+                                <div class="progess-bar" style="width: 92%;"></div>
+                            </div>
+                            <p>92% of unique visitor</p>
+                        </div>
+
+                    </div>
+                </div><!-- Top Report End -->
+
+                <!-- Top Report Start -->
+                <div class="col-xlg-3 col-md-6 col-12 mb-30">
+                    <div class="top-report">
+
+                        <!-- Head -->
+                        <div class="head">
+                            <h4>Product Sold</h4>
+                            <a href="#" class="view"><i class="zmdi zmdi-eye"></i></a>
+                        </div>
+
+                        <!-- Content -->
+                        <div class="content">
+                            <h5>Todays</h5>
+                            <h2>85,000.00</h2>
+                        </div>
+
+                        <!-- Footer -->
+                        <div class="footer">
+                            <div class="progess">
+                                <div class="progess-bar" style="width: 98%;"></div>
+                            </div>
+                            <p>98% of unique visitor</p>
+                        </div>
+
+                    </div>
+                </div><!-- Top Report End -->
+
+                <!-- Top Report Start -->
+                <div class="col-xlg-3 col-md-6 col-12 mb-30">
+                    <div class="top-report">
+
+                        <!-- Head -->
+                        <div class="head">
+                            <h4>Order Received</h4>
+                            <a href="#" class="view"><i class="zmdi zmdi-eye"></i></a>
+                        </div>
+
+                        <!-- Content -->
+                        <div class="content">
+                            <h5>Todays</h5>
+                            <h2>5,000.00</h2>
+                        </div>
+
+                        <!-- Footer -->
+                        <div class="footer">
+                            <div class="progess">
+                                <div class="progess-bar" style="width: 88%;"></div>
+                            </div>
+                            <p>88% of unique visitor</p>
+                        </div>
+
+                    </div>
+                </div><!-- Top Report End -->
+
+                <!-- Top Report Start -->
+                <div class="col-xlg-3 col-md-6 col-12 mb-30">
+                    <div class="top-report">
+
+                        <!-- Head -->
+                        <div class="head">
+                            <h4>Total Revenue</h4>
+                            <a href="#" class="view"><i class="zmdi zmdi-eye"></i></a>
+                        </div>
+
+                        <!-- Content -->
+                        <div class="content">
+                            <h5>Todays</h5>
+                            <h2>3,000,000.00</h2>
+                        </div>
+
+                        <!-- Footer -->
+                        <div class="footer">
+                            <div class="progess">
+                                <div class="progess-bar" style="width: 76%;"></div>
+                            </div>
+                            <p>76% of unique visitor</p>
+                        </div>
+
+                    </div>
+                </div><!-- Top Report End -->
+            </div><!-- Top Report Wrap End -->
+
+            <div class="row mbn-30">
+
+            </div>
+
+        </div><!-- Content Body End -->
+
+        <!-- Footer Section Start -->
+        <div class="footer-section">
+            <div class="container-fluid">
+
+                <div class="footer-copyright text-center">
+                    <p class="text-body-light">2019 &copy; <a href="https://themeforest.net/user/codecarnival">Codecarnival</a></p>
+                </div>
+
+            </div>
+        </div><!-- Footer Section End -->
+
+    </div>
+
+    <!-- JS
+============================================ -->
+
+    <!-- Global Vendor, plugins & Activation JS -->
+    <script src="assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="assets/js/vendor/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="assets/js/vendor/bootstrap.min.js"></script>
+    <!--Plugins JS-->
+    <script src="assets/js/plugins/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/plugins/tippy4.min.js.js"></script>
+    <!--Main JS-->
+    <script src="assets/js/main.js"></script>
+
+    <!-- Plugins & Activation JS For Only This Page -->
+
+    <!--Moment-->
+    <script src="assets/js/plugins/moment/moment.min.js"></script>
+
+    <!--Daterange Picker-->
+    <script src="assets/js/plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="assets/js/plugins/daterangepicker/daterangepicker.active.js"></script>
+
+    <!--Echarts-->
+    <script src="assets/js/plugins/chartjs/Chart.min.js"></script>
+    <script src="assets/js/plugins/chartjs/chartjs.active.js"></script>
+
+    <!--VMap-->
+    <script src="assets/js/plugins/vmap/jquery.vmap.min.js"></script>
+    <script src="assets/js/plugins/vmap/maps/jquery.vmap.world.js"></script>
+    <script src="assets/js/plugins/vmap/maps/samples/jquery.vmap.sampledata.js"></script>
+    <script src="assets/js/plugins/vmap/vmap.active.js"></script>
+
+</body>
+
+</html>
